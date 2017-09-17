38dff53b075157fee940387202c023dc300f2c3c
mdeejay <mdjrussia@gmail.com>
2017-09-17 16:20:24 +0700

    Update media_codecs / display

--- product/display.mk
+++ product/display.mk
@@ -1,10 +1,11 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Display
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
+#PRODUCT_PACKAGES += \
+#    libjni_livedisplay
 
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
